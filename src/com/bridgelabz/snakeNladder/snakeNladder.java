00503ea53def767b667fa19605754f0b78ddc3e3
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-27 14:44:21 +0530

    ADDED:players condition

--- src/com/bridgelabz/snakeNladder/snakeNladder.java
+++ src/com/bridgelabz/snakeNladder/snakeNladder.java
@@ -5,10 +5,10 @@ public class snakeNladder {
     public static final int LADDER = 1;
 	public static final int SNAKE = 2;
 	public static final int START = 0;
-	   	  public static void main(String[] args){
+	public static void main(String[] args){
 	      int position = 0;
 	      System.out.println("Starting position of player is: " + START);
-	      while(position <= 100){
+	      while(position < 100){
 	      int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 	      System.out.println("Number on rolled dice is : " +roll);
 	      int condition = (int) (Math.floor(Math.random() * 10) % 3);
@@ -19,15 +19,27 @@ public class snakeNladder {
 	                  break;
 	            case LADDER:
 	                  position = position + roll;
+	                  if(position <= 100)
 	                  System.out.println("New Position is: " + position);
+	                  else{
+							position = position - roll;
+	                  System.out.println("Remains at same position: " + position);
+							}
 	                  break;
 	            case SNAKE:
 	                  position = position - roll;
-	                  if (position >= 0 )
-	                  System.out.println("New Position is: " + position);
-	                  else
+	                  if (position <= 100){
+	                  if (position >= 0 ){
+	                  System.out.println("New Position is:" + position);
+							}
+	                  else{
 	                  position = 0;
 	                  System.out.println("New Position is: " + position);
+	                  }
+							}
+	                  else{
+	                  System.out.println("New position is:" + position);
+							}
 	                  break;
 	            default:
 	                  System.out.println("Default");
@@ -35,9 +47,9 @@ public class snakeNladder {
 	         }
 	      }
 	      System.out.println();
-	      if (position >= 100)
+	      if (position == 100)
 	      System.out.println("**");
-	      System.out.println("    PLAYER WON");
+	      System.out.println("PLAYER WON");
 	      System.out.println("**");
 	      }
-}
+	}
