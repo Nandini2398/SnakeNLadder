16fd53a6de9171add9a27e12c25be871cc829054
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-27 14:56:00 +0530

    ADDED:how many times rolled a diece

--- src/com/bridgelabz/snakeNladder/snakeNladder.java
+++ src/com/bridgelabz/snakeNladder/snakeNladder.java
@@ -5,56 +5,116 @@ public class snakeNladder {
     public static final int LADDER = 1;
 	public static final int SNAKE = 2;
 	public static final int START = 0;
-		 public static void main(String[] args){
-		      int position = 0, condition1 = 0, condition2 = 0, condition3 = 0, condition4 = 0;
+	public static void main(String[] args){
+			 int pos = 0, pos1 = 0, cntr1 = 0, cntr2 = 0, cntr3 = 0, cntr4 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0;
 		      System.out.println("Starting position of player is: " + START);
-		      while(position < 100){
+		      while(pos < 100 && pos1 < 100){
+				int con = 1;
+		a:		while(con == 1){
+				System.out.println("PLAYER 1");
 		      int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-			  condition1++;
+				cntr1++;
 		      System.out.println("Number on rolled dice is : " +roll);
-		      int condition = (int) (Math.floor(Math.random() * 10) % 3);
-		      System.out.println("Condition: " +condition);
-		      switch (condition){
+		      con = (int) (Math.floor(Math.random() * 10) % 3);
+		      System.out.println("Condition: " +con);
+		      switch (con){
 		            case NO_PLAY:
-		                  System.out.println("New Position is: " + position);
-		                  condition4++;
+								System.out.println("NO PLAY");
+		                  System.out.println("New Position is: " + pos);
+								cntr4++;
 		                  break;
 		            case LADDER:
-		                  position = position + roll;
-		                  if(position <= 100)
-		                  System.out.println("New Position is: " + position);
+								System.out.println("Got LADDER");
+		                  pos = pos + roll;
+		                  if(pos <= 100)
+		                  System.out.println("New Position is: " + pos);
 		                  else{
-		                  position = position - roll;
-		                  System.out.println("Remains at same position: " + position);
+								pos = pos - roll;
+		                  System.out.println("Remains at same position: " + pos);
 								}
-		                  condition2++;
-		                  break;
+								cntr2++;
+								continue a;
 		            case SNAKE:
-		                  position = position - roll;
-		                  if (position <= 100){
-		                  if (position >= 0 ){
-		                  System.out.println("New Position is: " + position);
+								System.out.println("Got SNAKE");
+		                  pos = pos - roll;
+		                  if (pos <= 100){
+		                  if (pos >= 0 ){
+		                  System.out.println("New Position is: " + pos);
 								}
 		                  else{
-		                  position = 0;
-		                  System.out.println("New Position is: " + position);
+		                  pos = 0;
+		                  System.out.println("New Position is: " + pos);
 		                  }
 								}
 		                  else{
-		                  System.out.println("New position is:" + position);
+		                  System.out.println("New position is:" + pos);
 								}
-						  condition3++;
+								cntr3++;
 		                  break;
 		            default:
 		                  System.out.println("Default");
 		                  break;
 		         }
+				}
+				int con1 = 1;
+		b:		while(con1 == 1 && pos1 < 100){
+				System.out.println("PLAYER 2");
+				int roll1 = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		      c1++;
+		      System.out.println("Number on rolled dice is : " +roll1);
+		      con1 = (int) (Math.floor(Math.random() * 10) % 3);
+		      System.out.println("Condition: " +con1);
+		      switch (con1){
+		            case NO_PLAY:
+								System.out.println("NO PLAY");
+		                  System.out.println("New Position is: " + pos1);
+		                  c4++;
+		                  break;
+		            case LADDER:
+								System.out.println("Got LADDER");
+		                  pos1 = pos1 + roll1;
+		                  if(pos1 <= 100)
+		                  System.out.println("New Position is: " + pos1);
+		                  else{
+		                  pos1 = pos1 - roll1;
+		                  System.out.println("Remains at same position: " + pos1);
+		                  }
+		                  c2++;
+								continue b;
+		            case SNAKE:
+								System.out.println("Got SNAKE");
+		                  pos1 = pos1 - roll1;
+		                  if (pos1 <= 100){
+		                  if (pos1 >= 0 ){
+		                  System.out.println("New Position is: " + pos1);
+		                  }
+		                  else{
+		                  pos1 = 0;
+		                  System.out.println("New Position is: " + pos1);
+		                  }
+		                  }
+		                  else{
+		                  System.out.println("New position is:" + pos1);
+		                  }
+		                  c3++;
+		                  break;
+		            default:
+		                  System.out.println("Default");
+		                  break;
+		       	  }
+					}
+				}
+		      System.out.println();
+				System.out.println("");
+		      if (pos == 100){
+		      System.out.println("Player1 ROLLED DICE "+ cntr1 +" time and got SNAKE "+ cntr3 +" times and got LADDER "+ cntr2 +" times and got NO PLAY "+ cntr4 +" times to win the game");
+		      System.out.println("    PLAYER1 WON");
+				}
+				else{
+		      System.out.println("Player2 ROLLED DICE "+ c1 +" time and got SNAKE "+ c3 +" times and got LADDER "+ c2 +" times and got NO PLAY "+c4+" times to win the game");
+				System.out.println("    PLAYER2 WON");
+				}
+		      System.out.println("");
+
 		      }
-		      			  System.out.println();
-				          System.out.println("Player ROLLED DICE "+condition1+" time and got SNAKE "+condition3+" times and got LADDER "+condition2+" times and got NO PLAY "+condition4+" times to win the game");
-				          if (position == 100)
-				          System.out.println("***");
-		                  System.out.println("    PLAYER WON");
-		                  System.out.println("***");
-		      }
-}
+		}
